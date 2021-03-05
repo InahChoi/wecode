a46d894810cc2a017bf820930b7a91a193aaa225
InahChoi <hyejooworld@gmail.com>
2021-03-05 14:08:38 +0900

    github python test file

---
+++ test.py
@@ -0,0 +1,11 @@
+def integer_number():
+    num_list = []
+    for i in range(1, 51):
+        if i % 2 == 0:
+            num_list.append(i)
+        else:
+            False
+        # return num
+    return num_list
+
+print(integer_number())
